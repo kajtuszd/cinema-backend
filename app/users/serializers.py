940a0fce0da8dded6386c1d4720a6f14d04d8a35
kajetan <kajetanzdanowicz@wp.pl>
2022-11-26 02:03:06 +0100

    improved permissions for users

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    is_moderator = serializers.BooleanField(read_only=True)
+
     class Meta:
         model = User
         fields = [
--- app/users/views.py
+++ app/users/views.py
@@ -1,13 +1,19 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 
 from users.models import User
 
+from .permissions import IsModerator, IsModeratorOrOwner
 from .serializers import UserSerializer
-from .permissions import IsModerator
 
 
 class UserListView(ListAPIView):
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated, IsModerator]
+    permission_classes = [IsModerator]
     serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsModeratorOrOwner]
+    serializer_class = UserSerializer
+    lookup_field = 'username'
